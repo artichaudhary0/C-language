76558a0f3ce299d411c12a4303f2a667d1118c81
artichaudhary0 <chaudhary.aarti1998@gmail.com>
2024-01-10 16:26:57 +0530

    third commit

---
+++ 6 constant.c
@@ -0,0 +1,30 @@
+/*
+    C O N S T A N T S :
+    Literal/Constants  are those whose value can't be canged during the 
+    execution of the program. in other words we can say that these are fixed values.
+
+    It is used where we want to fix value that can't be changed. When we try to change 
+    its value gives an error stating Constants values can't be changed.
+
+    Syntax for declaring a constants :
+         Using "const" keyword followed by data type and variable name.
+                        const int a = 10;
+
+
+    S Y M B O L I C    C O N S T A N T S :  
+    If we want to create a constant having a specified name and can be used anywhere in the program
+    then we use the concept of symbolic constant.
+    Symbolic constants are not stored in memory, they are replaced with their respective values at compile time.
+
+    Syntax : #define name value
+                      ex : #define PI 3.1415
+                           #define name "arti chaudhary" 
+
+    Note :
+            1) Every symbolic constant starts with #define.
+            2) Usually the constant name is in upper case.
+            3) No semi colon is there at the end.
+    
+
+            
+*/
---
+++ 7 punctuators_seperators_delimiters.c
@@ -0,0 +1,15 @@
+/*
+
+    P U N C T U A T O R S   /  S E P E R A T O R S  /  D E L I M I T E R S :
+    Delimiters are those who have some pre defined meaning and 
+    significance.
+
+    The difference punctuators are :
+    [] - Used for array declaration.
+    {} - Used for block of statement.
+    () - Used for expression / condition.
+    ;  - Used for end of the statement
+    :  - Used for a label.
+    ,  - Used for separating variables.
+    #  - Used for declaring preprocessor directive
+*/
